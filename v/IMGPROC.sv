35eeff7b6417916dfb4d294a2de42bb06bdaa0b0
ztsimons <ztsimons@ad.engr.wisc.edu>
2025-02-04 18:54:53 -0600

    IMGPROC

--- v/IMGPROC.sv
+++ v/IMGPROC.sv
@@ -1,31 +1,61 @@
-module IMGPROC(	
-	tap0,
-    tap1,
-    clk,
-    rst,
-    DVAL
+module IMGPROC(	oRed,
+oGreen,
+oBlue,
+oDVAL,
+iX_Cont,
+iY_Cont,
+iDATA,
+iDVAL,
+iCLK,
+iRST
 );
 
-    input [11:0] tap0, tap1;
-    input clk, rst, DVAL;
+input	[10:0]	iX_Cont;
+input	[10:0]	iY_Cont;
+input	[11:0]	iDATA;
+input			iDVAL;
+input			iCLK;
+input			iRST;
+output	[11:0]	oRed;
+output	[11:0]	oGreen;
+output	[11:0]	oBlue;
+output			oDVAL;
+wire	[11:0]	mDATA_0;
+wire	[11:0]	mDATA_1;
 
-    logic [11:0] gs_out, conv_out;
+logic   [11:0]  shift_out;
+logic   [11:0]  gs_out, conv_out;
 
-    greyscale greyscale(
-        .data_in_2(tap1), 
-        .data_in_1(tap0), 
-        .clk(clk), 
-        .rst(rst), 
+assign oRed = conv_out;
+assign oBlue = conv_out;
+assign oGreen = conv_out;
+assign oDVAL = ???;
+
+
+Line_Buffer1 	u0	(	.clken(iDVAL),
+        .clock(iCLK),
+        .shiftin(iDATA),
+        .taps0x(mDATA_1),
+        .taps1x(mDATA_0),	
+        .shiftout(shift_out)
+        );
+
+greyscale greyscale(
+        .data_in_2(mDATA_0), 
+        .data_in_1(mDATA_1), 
+        .clk(iCLK), 
+        .rst(iRST), 
         .data_out(gs_out)
-    );
+        );
 
-    convolution convolution(
-        .iCLK(clk),
-        .iRST(rst),
-        .iDVAL(DVAL),
+ convolution convolution(
+        .iCLK(iCLK),
+        .iRST(iRST),
+        .iDVAL(iDVAL),
         .vertical(???),
         .row_2_col_0(gs_out),
         .pixel_out(conv_out)
-    );
+        );
+
 
 endmodule
